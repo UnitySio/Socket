a152ef58fb5a6cc57acb31d36af2b7e7e23bc437
UnitySio <dnwls010728@gmail.com>
2024-01-22 15:38:37 +0900

    CHORE: 누락 파일 추가

--- Client/Game/MainScene.h
+++ Client/Game/MainScene.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 #include "../Engine/Scene/Scene.h"
+#include "box2d/b2_math.h"
+#include "box2d/b2_world.h"
 
 class MainScene : public Scene
 {
@@ -11,5 +13,7 @@ public:
     virtual void End() final;
     virtual void Tick(float deltaTime) final;
     virtual void Render() final;
+
+private:
     
 };
