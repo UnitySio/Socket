1821aab9eaf3a8166467f42ddb2e7f9b11405b29
UnitySio <dnwls010728@gmail.com>
2024-02-15 17:49:21 +0900

    FEAT: Vector 추가

---
+++ Client/Engine/Vector.cpp
@@ -0,0 +1,223 @@
+﻿#include "Vector.h"
+
+#include <algorithm>
+
+Vector::Vector() :
+    x(),
+    y()
+{
+}
+
+Vector::Vector(float x, float y) :
+    x(x),
+    y(y)
+{
+}
+
+Vector& Vector::operator+=(const Vector& kVector)
+{
+    x += kVector.x;
+    y += kVector.y;
+
+    return *this;
+}
+
+Vector& Vector::operator-=(const Vector& kVector)
+{
+    x -= kVector.x;
+    y -= kVector.y;
+
+    return *this;
+}
+
+Vector& Vector::operator*=(const Vector& kVector)
+{
+    x *= kVector.x;
+    y *= kVector.y;
+
+    return *this;
+}
+
+Vector& Vector::operator/=(const Vector& kVector)
+{
+    x /= kVector.x;
+    y /= kVector.y;
+
+    return *this;
+}
+
+Vector& Vector::operator=(const Vector& kVector)
+{
+    x = kVector.x;
+    y = kVector.y;
+
+    return *this;
+}
+
+Vector Vector::operator+(const Vector& kVector)
+{
+    return { x + kVector.x, y + kVector.y };
+}
+
+Vector Vector::operator-(const Vector& kVector)
+{
+    return { x - kVector.x, y - kVector.y };
+}
+
+Vector Vector::operator*(const Vector& kVector)
+{
+    return { x * kVector.x, y * kVector.y };
+}
+
+Vector Vector::operator/(const Vector& kVector)
+{
+    return { x / kVector.x, y / kVector.y };
+}
+
+Vector Vector::operator+(float val)
+{
+    return { x + val, y + val };
+}
+
+Vector Vector::operator-(float val)
+{
+    return { x - val, y - val };
+}
+
+Vector Vector::operator*(float val)
+{
+    return { x * val, y * val };
+}
+
+Vector Vector::operator/(float val)
+{
+    return { x / val, y / val };
+}
+
+Vector Vector::Zero()
+{
+    return { 0.f, 0.f };
+}
+
+Vector Vector::One()
+{
+    return { 1.f, 1.f };
+}
+
+Vector Vector::Left()
+{
+    return { -1.f, 0.f };
+}
+
+Vector Vector::Up()
+{
+    return { 0.f, -1.f };
+}
+
+Vector Vector::Right()
+{
+    return { 1.f, 0.f };
+}
+
+Vector Vector::Down()
+{
+    return { 0.f, 1.f };
+}
+
+Vector Vector::Lerp(Vector a, Vector b, float t)
+{
+    t = std::clamp(t, 0.f, 1.f);
+    return a + (b - a) * t;
+}
+
+float Vector::Distance(Vector a, Vector b)
+{
+    Vector diff = a - b;
+    return diff.Magnitude();
+}
+
+float Vector::Dot(Vector a, Vector b)
+{
+    const float theta = (a.x * b.x) + (a.y * b.y);
+    return theta;
+}
+
+Vector Vector::Normalized()
+{
+    float m = Magnitude();
+
+    if (m > 0)
+    {
+        return { x / m, y / m };
+    }
+
+    return Zero();
+}
+
+bool Vector::operator==(const Vector& kVector) const
+{
+    if (fabsf(x - kVector.x) <= FLT_EPSILON &&
+        fabsf(y - kVector.y) <= FLT_EPSILON)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool Vector::operator!=(const Vector& kVector) const
+{
+    if (fabsf(x - kVector.x) > FLT_EPSILON ||
+        fabsf(y - kVector.y) > FLT_EPSILON)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool Vector::operator==(float val) const
+{
+    if (fabsf(x - val) <= FLT_EPSILON &&
+        fabsf(y - val) <= FLT_EPSILON)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool Vector::operator!=(float val) const
+{
+    if (fabsf(x - val) > FLT_EPSILON ||
+        fabsf(y - val) > FLT_EPSILON)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+float Vector::Magnitude()
+{
+    const auto temp_x = static_cast<double>(x);
+    const auto temp_y = static_cast<double>(y);
+
+    return static_cast<float>(sqrt(pow(temp_x, 2) + pow(temp_y, 2)));
+}
+
+void Vector::Normalize()
+{
+    float m = Magnitude();
+
+    if (m > 0)
+    {
+        x /= m;
+        y /= m;
+    }
+    else
+    {
+        x = 0.f;
+        y = 0.f;
+    }
+}
---
+++ Client/Engine/Vector.h
@@ -0,0 +1,111 @@
+﻿#pragma once
+
+class Vector
+{
+public:
+    Vector();
+    Vector(float x, float y);
+    Vector& operator+=(const Vector& kVector);
+    Vector& operator-=(const Vector& kVector);
+    Vector& operator*=(const Vector& kVector);
+    Vector& operator/=(const Vector& kVector);
+    Vector& operator=(const Vector& kVector);
+
+    Vector operator+(const Vector& kVector);
+    Vector operator-(const Vector& kVector);
+    Vector operator*(const Vector& kVector);
+    Vector operator/(const Vector& kVector);
+
+    Vector operator+(float val);
+    Vector operator-(float val);
+    Vector operator*(float val);
+    Vector operator/(float val);
+ 
+    ~Vector() = default;
+ 
+    float x;
+    float y;
+
+    /**
+     * \brief Vector(0.f, 0.f)
+     * \return Vector
+     */
+    static Vector Zero();
+
+    /**
+     * \brief Vector(1.f, 1.f)
+     * \return Vector
+     */
+    static Vector One();
+
+    /**
+     * \brief Vector(-1.f, 0.f)
+     * \return Vector
+     */
+    static Vector Left();
+
+    /**
+     * \brief Vector(0.f, -1.f)
+     * \return Vector
+     */
+    static Vector Up();
+
+    /**
+     * \brief Vector(1.f, 0.f)
+     * \return Vector
+     */
+    static Vector Right();
+
+    /**
+     * \brief Vector(0.f, 1.f)
+     * \return Vector
+     */
+    static Vector Down();
+
+    /**
+     * \brief 두 백터를 선형 보간합니다.
+     * \param a 첫번째 백터
+     * \param b 두번째 백터
+     * \param t 비율
+     * \return Vector
+     */
+    static Vector Lerp(Vector a, Vector b, float t);
+ 
+    /**
+     * \brief 두 백터의 거리를 반환합니다.
+     * \param a 첫번째 백터
+     * \param b 두번째 백터
+     * \return float
+     */
+    static float Distance(Vector a, Vector b);
+
+    /**
+     * \brief 두 백터의 내적을 반환합니다.
+     * \param a 첫번째 백터
+     * \param b 두번째 백터
+     * \return float
+     */
+    static float Dot(Vector a, Vector b);
+ 
+    /**
+     * \brief 백터를 정규화된 값으로 반환합니다.
+     * \return Vector
+     */
+    Vector Normalized();
+
+    bool operator==(const Vector& kVector) const;
+    bool operator!=(const Vector& kVector) const;
+    bool operator==(float val) const;
+    bool operator!=(float val) const;
+
+    /**
+     * \brief 백터의 길이를 반환합니다.
+     * \return float
+     */
+    float Magnitude();
+
+    /**
+     * \brief 백터를 정규화를 합니다.
+     */
+    void Normalize();
+};
