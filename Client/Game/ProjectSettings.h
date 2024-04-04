a89887ac5661fc52490d1e3ccb4a48647512e000
UnitySio <dnwls010728@gmail.com>
2024-04-04 17:36:58 +0900

    FEAT: ProjectSettings 주석 추가

--- Client/Game/ProjectSettings.h
+++ Client/Game/ProjectSettings.h
@@ -18,17 +18,17 @@ enum ActorLayer : uint16
 class ProjectSettings
 {
 public:
-    static inline const std::wstring kProjectName = L"Game";
-    static inline const std::wstring kProjectClassName = L"Game_GE";
-    static inline const std::wstring kProjectVersion = L"1.0.0";
+    static inline const std::wstring kProjectName = L"Game"; // 프로젝트 이름
+    static inline const std::wstring kProjectClassName = L"Game_GE"; // 프로젝트 클래스 이름
+    static inline const std::wstring kProjectVersion = L"1.0.0"; // 프로젝트 버전
 
-    static inline constexpr int kScreenWidth = 640;
-    static inline constexpr int kScreenHeight = 480;
+    static inline constexpr int kScreenWidth = 640; // 화면 가로 크기
+    static inline constexpr int kScreenHeight = 480; // 화면 세로 크기
     
-    static inline constexpr bool kShowFrameRate = true;
-    static inline constexpr bool kUseVSync = true;
+    static inline constexpr bool kShowFrameRate = true; // 프레임레이트 표시 여부
+    static inline constexpr bool kUseVSync = true; // 수직동기화 사용 여부
 
-    static inline const std::map<uint16, uint16> kLayerCollisionMatrix = {
+    static inline const std::map<uint16, uint16> kLayerCollisionMatrix = { // 레이어 충돌 매트릭스
         {kDefault, kDefault | kPlayer},
         {kPlayer, kPlayer | kDefault},
     };
