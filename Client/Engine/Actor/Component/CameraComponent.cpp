c418aa9da2bede1c0d3857949a1da1eddfddfc5b
Woojin Choi <dnwls010728@gmail.com>
2024-05-06 23:02:41 +0900

    FEAT: CameraComponent 개선

--- Client/Engine/Actor/Component/CameraComponent.cpp
+++ Client/Engine/Actor/Component/CameraComponent.cpp
@@ -1,6 +1,7 @@
 ﻿#include "CameraComponent.h"
 
 #include "TransformComponent.h"
+#include "Actor/Actor.h"
 #include "Level/World.h"
 #include "Misc/EngineMacros.h"
 #include "Windows/D3D/Renderer.h"
@@ -13,10 +14,12 @@ void CameraComponent::TickComponent(float delta_time)
 {
     ActorComponent::TickComponent(delta_time);
 
+    TransformComponent* transform = GetOwner()->GetTransform();
+    Math::Vector2 location = transform->GetWorldLocation();
+
     if (Viewport* viewport = g_renderer->FindViewport(GetWorld()->GetWindow()))
     {
-        viewport->view_matrix = DirectX::XMMatrixTranslation(-0.f, -0.f, 0.f)
-            * DirectX::XMMatrixRotationRollPitchYaw(-0.f, -0.f, -0.f);
+        viewport->view_matrix = DirectX::XMMatrixTranslation(-location.x, -location.y, 0.f)
+            * DirectX::XMMatrixRotationRollPitchYaw(0.f, 0.f, -transform->GetWorldRotationZ());
     }
-
 }
