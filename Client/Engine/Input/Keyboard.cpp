1743380b07ebdfc352a6825c5618e0720094759e
UnitySio <dnwls010728@gmail.com>
2024-05-08 18:14:33 +0900

    FEAT: Keyboard 개선

--- Client/Engine/Input/Keyboard.cpp
+++ Client/Engine/Input/Keyboard.cpp
@@ -1,34 +1,9 @@
 ﻿#include "Keyboard.h"
 
-#include <Windows.h>
-
 Keyboard::Keyboard()
 {
 }
 
-void Keyboard::BindAction(MathTypes::uint32 key, Action action)
+Keyboard::~Keyboard()
 {
-    actions_[key] = action;
-}
-
-void Keyboard::Tick()
-{
-    for (auto& action : actions_)
-    {
-        if (GetAsyncKeyState(action.first) & 0x8000)
-        {
-            if (action.second.is_down)
-                action.second.Ongoing();
-            else action.second.Started();
-            
-            action.second.is_down = true;
-        }
-        else
-        {
-            if (action.second.is_down)
-                action.second.Canceled();
-            
-            action.second.is_down = false;
-        }
-    }
 }
--- Client/Engine/Input/Keyboard.h
+++ Client/Engine/Input/Keyboard.h
@@ -1,28 +1,9 @@
 ﻿#pragma once
-#include <functional>
-#include <map>
-
-#include "Math/MathTypes.h"
-
-struct Action
-{
-    bool is_down = false;
-    
-    std::function<void()> Started;
-    std::function<void()> Ongoing;
-    std::function<void()> Canceled;
-};
 
 class Keyboard
 {
 public:
     Keyboard();
-    ~Keyboard() = default;
-
-    void BindAction(MathTypes::uint32 key, Action action);
-    void Tick();
-
-private:
-    std::map<MathTypes::uint32, Action> actions_;
+    ~Keyboard();
     
 };
