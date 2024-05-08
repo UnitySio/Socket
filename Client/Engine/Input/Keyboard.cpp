73ede0dd91698000a8390c09238915bd65b3c45c
UnitySio <dnwls010728@gmail.com>
2024-05-08 18:29:38 +0900

    FEAT: Keyboard 개선

--- Client/Engine/Input/Keyboard.cpp
+++ Client/Engine/Input/Keyboard.cpp
@@ -1,9 +1,15 @@
 ﻿#include "Keyboard.h"
 
+#include <Windows.h>
+
 Keyboard::Keyboard()
 {
 }
 
-Keyboard::~Keyboard()
+void Keyboard::BindAxis(MathTypes::uint32 key_code)
+{
+}
+
+void Keyboard::BindAction(MathTypes::uint32 key_code)
 {
 }
--- Client/Engine/Input/Keyboard.h
+++ Client/Engine/Input/Keyboard.h
@@ -1,9 +1,19 @@
 ﻿#pragma once
+#include <functional>
+#include <map>
+
+#include "Math/MathTypes.h"
 
 class Keyboard
 {
 public:
     Keyboard();
-    ~Keyboard();
+    ~Keyboard() = default;
+
+    void BindAxis(MathTypes::uint32 key_code);
+    void BindAction(MathTypes::uint32 key_code);
+
+private:
+    std::map<MathTypes::uint32, std::function<void(float)>> axis_map_;
     
 };
