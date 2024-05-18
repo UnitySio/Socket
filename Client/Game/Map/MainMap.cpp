eeb83a95c0c4d789c87f94920a1cec69027198d9
Ogdoabyss <101399129+YingHi@users.noreply.github.com>
2024-05-19 05:00:34 +0900

    fmod 오류 수정 및 Reflection 오류 수정

--- Client/Game/Map/MainMap.cpp
+++ Client/Game/Map/MainMap.cpp
@@ -3,18 +3,29 @@
 #include "../Actor/FollowCamera.h"
 #include "../Actor/Floor.h"
 #include "../Actor/Pawn.h"
+#include "../Reflection.h"
 
 MainMap::MainMap(const std::wstring& kName) : Level(kName)
 {
-    Actor* follow_camera = new FollowCamera(GetWorld(), L"FollowCamera");
+    FollowCamera* follow_camera = new FollowCamera(GetWorld(), L"FollowCamera");
     AddActor(follow_camera);
     
-    Actor* floor = new Floor(GetWorld(), L"Floor");
+    Floor* floor = new Floor(GetWorld(), L"Floor");
     AddActor(floor);
 
-    Actor* pawn = new Pawn(GetWorld(), L"Pawn");
+    Pawn* pawn = new Pawn(GetWorld(), L"Pawn");
     AddActor(pawn);
-    
-    FollowCamera* camera = dynamic_cast<FollowCamera*>(follow_camera);
-    camera->SetTarget(pawn);
+
+    Reflection ref(follow_camera);
+
+    Reflection ref1(floor);
+
+    Reflection ref2(pawn);
+
+    //ref.typePointer->GetType();
+
+    ref.typePointer->SetTarget(pawn);
+
+    /*FollowCamera* camera = dynamic_cast<FollowCamera*>(follow_camera);
+    camera->SetTarget(pawn);*/
 }
