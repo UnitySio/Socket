118685b43a1b31fcfdcd53d7c855ac120c77ff78
Woojin Choi <dnwls010728@gmail.com>
2024-06-06 20:45:37 +0900

    FEAT: Keyboard 수정

--- Client/Engine/Input/Keyboard.h
+++ Client/Engine/Input/Keyboard.h
@@ -1,13 +1,8 @@
 ﻿#pragma once
 #include <Windows.h>
 
-#include <vector>
 #include "Singleton.h"
 #include "Math/MathTypes.h"
-#include "Misc/Delegate.h"
-
-struct EventKeyboard;
-//enum class EventKeyboard::Keycode : MathTypes::uint8;
 
 class Keyboard : public Singleton<Keyboard>
 {
@@ -15,39 +10,6 @@ public:
     Keyboard();
     virtual ~Keyboard() override = default;
 
-    void RegisterKey(BYTE keycode);
-    void UnRegisterKey(BYTE keycode);
-
-    Delegate<void(const EventKeyboard& kE)> OnDown;
-    Delegate<void(const EventKeyboard& kE)> OnUp;
-    Delegate<void(const EventKeyboard& kE)> Pressed;
-
-
-private:
     bool ProcessMessage(UINT message, WPARAM wParam, LPARAM lParam, MathTypes::uint32 handler_result);
-    void OnKeyDown(const EventKeyboard& kE);
-    void OnKeyUp(const EventKeyboard& kE);
-    void Pressing(const EventKeyboard& kE);
-    std::vector<EventKeyboard> keys_;
-    friend class Core;
-};
-
-
-struct EventKeyboard
-{
-
-    enum class KeyState : MathTypes::uint8
-    {
-        Down,
-        Up,
-        Pressing
-    };
-
-    EventKeyboard(BYTE keycode)
-        : keyCode_(keycode), keyflag_(0), keyState_(KeyState::Up)
-    {};
     
-    BYTE keyCode_;
-    WORD keyflag_;
-    KeyState keyState_;
 };
