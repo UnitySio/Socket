1c1edec4ed03d49c76ffab6e7dcd4e9b4485db67
Woojin Choi <dnwls010728@gmail.com>
2024-06-23 11:36:42 +0900

    FEAT: Tilemap 일부 개선

--- Client/Engine/Actor/Component/Tilemap/TilemapLayer.cpp
+++ Client/Engine/Actor/Component/Tilemap/TilemapLayer.cpp
@@ -25,14 +25,14 @@ void TilemapLayer::AddShapes(const Math::Vector2& position, const Math::Vector2&
     }
 }
 
-TilemapChunk& TilemapLayer::GetChunk(int x, int y)
+TilemapChunk* TilemapLayer::GetChunk(int x, int y)
 {
     MathTypes::uint32 chunk_x = (x * map_tile_size_.x) / chunk_size_.x;
     MathTypes::uint32 chunk_y = (y * map_tile_size_.y) / chunk_size_.y;
-    return *chunks_[chunk_y * chunk_count_.x + chunk_x];
+    return chunks_[chunk_y * chunk_count_.x + chunk_x].get();
 }
 
-TilemapChunk& TilemapLayer::GetChunk(int x, int y, Math::Vector2& tile_relative_position)
+TilemapChunk* TilemapLayer::GetChunk(int x, int y, Math::Vector2& tile_relative_position)
 {
     MathTypes::uint32 chunk_x = (x * map_tile_size_.x) / chunk_size_.x;
     MathTypes::uint32 chunk_y = (y * map_tile_size_.y) / chunk_size_.y;
@@ -40,7 +40,7 @@ TilemapChunk& TilemapLayer::GetChunk(int x, int y, Math::Vector2& tile_relative_
     tile_relative_position.x = ((x * map_tile_size_.x) - chunk_x * chunk_size_.x) / map_tile_size_.x;
     tile_relative_position.y = ((y * map_tile_size_.y) - chunk_y * chunk_size_.y) / map_tile_size_.y;
 
-    return *chunks_[chunk_y * chunk_count_.x + chunk_x];
+    return chunks_[chunk_y * chunk_count_.x + chunk_x].get();
 }
 
 void TilemapLayer::CreateChunks(const tmx::Map& map, const tmx::TileLayer& layer, const std::shared_ptr<Texture>& texture, const Math::Vector2& tile_size)
--- Client/Engine/Actor/Component/Tilemap/TilemapLayer.h
+++ Client/Engine/Actor/Component/Tilemap/TilemapLayer.h
@@ -16,8 +16,8 @@ public:
 
     void AddShapes(const Math::Vector2& position, const Math::Vector2& scale, const Math::Vector2& pivot = Math::Vector2::Zero());
 
-    TilemapChunk& GetChunk(int tile_x, int tile_y);
-    TilemapChunk& GetChunk(int tile_x, int tile_y, Math::Vector2& tile_relative_position);
+    TilemapChunk* GetChunk(int tile_x, int tile_y);
+    TilemapChunk* GetChunk(int tile_x, int tile_y, Math::Vector2& tile_relative_position);
 
 private:
     void CreateChunks(const tmx::Map& map, const tmx::TileLayer& layer, const SHARED_PTR<Texture>& texture, const Math::Vector2& tile_size);
