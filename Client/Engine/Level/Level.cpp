eb096c15f7c15ad0d7a06ba67afe01eef771d4e2
UnitySio <dnwls010728@gmail.com>
2024-07-05 17:47:39 +0900

    FEAT: 불필요한 코드 정리

--- Client/Engine/Level/Level.cpp
+++ Client/Engine/Level/Level.cpp
@@ -43,15 +43,6 @@ void Level::InitializeActors()
 
 void Level::PhysicsTick(float delta_time)
 {
-    // for (const auto& actor : actors_)
-    // {
-    //     if (!actor->is_active_ || actor->is_destroy_) continue;
-    //
-    //     b2Vec2 position = actor->body_->GetPosition();
-    //     actor->previous_location_ = {position.x, position.y};
-    //     actor->previous_angle_ = actor->body_->GetAngle();
-    // }
-
     for (const auto& actor : actors_)
     {
         if (!actor->is_active_ || actor->is_destroy_) continue;
