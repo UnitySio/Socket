99c22cd4de040fc1715720366f912cc3d3944011
UnitySio <dnwls010728@gmail.com>
2024-07-17 12:27:25 +0900

    FEAT: Audio 관련 개선

--- Client/Engine/Audio/AudioManager.h
+++ Client/Engine/Audio/AudioManager.h
@@ -15,12 +15,9 @@ class AudioManager : public Singleton<AudioManager>
 public:
     AudioManager();
     virtual ~AudioManager() override;
-
-    bool Init();
     
     bool AddSound(const std::wstring& kName, const std::wstring& kPath);
 
-    void Tick();
     void SetLoop(const std::wstring& kName, bool is_loop);
     void PlayOneShot(const std::wstring& kName, float volume = 1.f);
     void PauseSound(int id);
@@ -31,6 +28,12 @@ public:
     int PlaySound2D(const std::wstring& kName, FMOD_CHANNELGROUP* channel_group = nullptr);
 
 private:
+    friend class GameEngine;
+    
+    bool Init();
+    
+    void Tick();
+    
     class FMOD_SYSTEM* fmod_system_;
     FMOD_CHANNEL* channels_[MAX_CHANNEL_COUNT];
 
