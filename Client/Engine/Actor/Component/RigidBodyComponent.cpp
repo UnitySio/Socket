24abfd93ca5a09efe50d537218de5ce30c6e97a4
Woojin Choi <dnwls010728@gmail.com>
2024-08-26 23:05:06 +0900

    FEAT: RigidbodyComponent assert 전부 CHECK로 교체

--- Client/Engine/Actor/Component/RigidBodyComponent.cpp
+++ Client/Engine/Actor/Component/RigidBodyComponent.cpp
@@ -14,10 +14,10 @@ RigidBodyComponent::RigidBodyComponent(Actor* owner, const std::wstring& kName) 
 void RigidBodyComponent::SetBodyType(BodyType type)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (type)
     {
@@ -38,10 +38,10 @@ void RigidBodyComponent::SetBodyType(BodyType type)
 void RigidBodyComponent::SetGravityScale(float scale)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     b2Body_SetGravityScale(body_id, scale);
 }
@@ -49,10 +49,10 @@ void RigidBodyComponent::SetGravityScale(float scale)
 void RigidBodyComponent::SetSleepMode(SleepMode mode)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (mode)
     {
@@ -69,10 +69,10 @@ void RigidBodyComponent::SetSleepMode(SleepMode mode)
 void RigidBodyComponent::SetCollisionDetectionMode(CollisionDetectionMode mode)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (mode)
     {
@@ -89,10 +89,10 @@ void RigidBodyComponent::SetCollisionDetectionMode(CollisionDetectionMode mode)
 void RigidBodyComponent::SetFreezeRotation(bool freeze)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
     
     b2Body_SetFixedRotation(body_id, freeze);
 }
@@ -100,10 +100,10 @@ void RigidBodyComponent::SetFreezeRotation(bool freeze)
 void RigidBodyComponent::SetVelocity(const Math::Vector2& kVelocity)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
     
     b2Body_SetLinearVelocity(body_id, {kVelocity.x, kVelocity.y});
 }
@@ -111,10 +111,10 @@ void RigidBodyComponent::SetVelocity(const Math::Vector2& kVelocity)
 void RigidBodyComponent::SetAngularVelocity(float velocity)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     b2Body_SetAngularVelocity(body_id, velocity);
 }
@@ -122,10 +122,10 @@ void RigidBodyComponent::SetAngularVelocity(float velocity)
 void RigidBodyComponent::AddForce(const Math::Vector2& kForce, ForceMode mode)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (mode)
     {
@@ -142,10 +142,10 @@ void RigidBodyComponent::AddForce(const Math::Vector2& kForce, ForceMode mode)
 void RigidBodyComponent::AddForceAtPosition(const Math::Vector2& kForce, const Math::Vector2& kPosition, ForceMode mode)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (mode)
     {
@@ -162,10 +162,10 @@ void RigidBodyComponent::AddForceAtPosition(const Math::Vector2& kForce, const M
 void RigidBodyComponent::AddTorque(float torque, ForceMode mode)
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     switch (mode)
     {
@@ -182,10 +182,10 @@ void RigidBodyComponent::AddTorque(float torque, ForceMode mode)
 void RigidBodyComponent::Sleep()
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     b2Body_SetAwake(body_id, false);
 }
@@ -193,10 +193,10 @@ void RigidBodyComponent::Sleep()
 void RigidBodyComponent::WakeUp()
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     b2Body_SetAwake(body_id, true);
 }
@@ -204,10 +204,10 @@ void RigidBodyComponent::WakeUp()
 Math::Vector2 RigidBodyComponent::GetVelocity() const
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     b2Vec2 velocity = b2Body_GetLinearVelocity(body_id);
     return {velocity.x, velocity.y};
@@ -216,10 +216,10 @@ Math::Vector2 RigidBodyComponent::GetVelocity() const
 float RigidBodyComponent::GetAngularVelocity() const
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     return b2Body_GetAngularVelocity(body_id);
 }
@@ -227,10 +227,10 @@ float RigidBodyComponent::GetAngularVelocity() const
 bool RigidBodyComponent::IsAwake() const
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     return b2Body_IsAwake(body_id);
 }
@@ -238,10 +238,10 @@ bool RigidBodyComponent::IsAwake() const
 b2BodyType RigidBodyComponent::GetBodyType() const
 {
     Actor* owner = GetOwner();
-    assert(owner);
+    CHECK(owner);
 
     b2BodyId body_id = owner->body_id_;
-    assert(b2Body_IsValid(body_id));
+    CHECK(b2Body_IsValid(body_id));
 
     return b2Body_GetType(body_id);
 }
