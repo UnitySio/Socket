77d5df330167974e933d72b1385eb45ab4892a4c
UnitySio <dnwls010728@gmail.com>
2024-09-05 17:57:07 +0900

    FEAT: FileManager 추가

---
+++ Client/Engine/File/FileManager.cpp
@@ -0,0 +1,6 @@
+﻿#include "pch.h"
+#include "FileManager.h"
+
+FileManager::FileManager()
+{
+}
---
+++ Client/Engine/File/FileManager.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Singleton.h"
+
+#include <filesystem>
+
+class FileManager : public Singleton<FileManager>
+{
+public:
+    FileManager();
+    virtual ~FileManager() override = default;
+    
+};
