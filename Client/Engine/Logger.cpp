5298e8e164e7a522c3ba19304ec8d61b0954a2be
UnitySio <dnwls010728@gmail.com>
2024-09-05 11:16:25 +0900

    FEAT: Logger 추가

---
+++ Client/Engine/Logger.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "Logger.h"
+
+#include <cstdarg>
+#include <Windows.h>
+
+Logger::Logger()
+{
+}
+
+void Logger::Print(const wchar_t* message, ...)
+{
+    va_list args;
+    va_start(args, message);
+
+    wchar_t buffer[256];
+    vswprintf(buffer, sizeof(buffer), message, args);
+    wcscat_s(buffer, L"\n");
+    
+    OutputDebugString(buffer);
+    va_end(args);
+}
---
+++ Client/Engine/Logger.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Singleton.h"
+
+class Logger : public Singleton<Logger>
+{
+public:
+    Logger();
+    virtual ~Logger() override = default;
+
+    static void Print(const wchar_t* message, ...);
+    
+};
