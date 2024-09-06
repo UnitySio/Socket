e356d758b34744f91b84ba0c79b8ef43fa8d5acf
Woojin Choi <dnwls010728@gmail.com>
2024-09-06 21:49:27 +0900

    FEAT: AnimatorComponent 구조 개선

--- Client/Engine/Actor/Component/Animator/AnimatorComponent.cpp
+++ Client/Engine/Actor/Component/Animator/AnimatorComponent.cpp
@@ -10,7 +10,8 @@ AnimatorComponent::AnimatorComponent(Actor* owner, const std::wstring& kName) :
     sprite_renderer_(nullptr),
     clips_(),
     timer_(0.f),
-    current_index_(0)
+    current_index_(0),
+    is_playing_(false)
 {
     current_clip_ = nullptr;
 }
@@ -28,29 +29,26 @@ void AnimatorComponent::TickComponent(float delta_time)
     ActorComponent::TickComponent(delta_time);
     if (!sprite_renderer_) return;
 
-    timer_ += delta_time;
-    if (timer_ >= 1.f / current_clip_->GetFrameRate())
+    if (is_playing_)
     {
-        timer_ = 0.f; 
-        //if (!IsPlaying()) LOG(L"Animation is end");
-        if (current_index_ < current_clip_->frames_.size() - 1)
+        timer_ += delta_time;
+        if (timer_ >= 1.f / current_clip_->GetFrameRate())
         {
-            is_playing_ = true;
-            current_index_++;
-            sprite_renderer_->frame_index_ = current_clip_->frames_[current_index_];
+            if (current_clip_->events_.contains(current_index_)) current_clip_->events_[current_index_]();
+            if (current_clip_->is_repeat_ || current_index_ < current_clip_->frames_.size() - 1)
+            {
+                current_index_ = (current_index_ + 1) % current_clip_->frames_.size();
+                sprite_renderer_->frame_index_ = current_clip_->frames_[current_index_];
+            
+                timer_ -= 1.f / current_clip_->GetFrameRate();
+            }
+            else
+            {
+                is_playing_ = false;
+                timer_ = 0.f;
+                current_index_ = 0;
+            }
         }
-        else if(current_clip_->is_repeat_)
-        {
-            current_index_ = 0;
-            sprite_renderer_->frame_index_ = current_clip_->frames_[0];
-        }
-        else
-        {
-            is_playing_ = false;
-            return;
-        }
-
-        if (current_clip_->events_.contains(current_index_)) current_clip_->events_[current_index_]();
     }
 }
 
@@ -70,15 +68,19 @@ std::shared_ptr<AnimationClip> AnimatorComponent::AddClip(const std::wstring& kN
 bool AnimatorComponent::PlayClip(std::wstring clip_name)
 {
     if (clips_[clip_name] == current_clip_) return false;
-    current_index_ = 0;
     current_clip_ = clips_[clip_name];
+    is_playing_ = true;
+    timer_ = 0.f;
+    current_index_ = 0;
     return true;
 }
 
 bool AnimatorComponent::PlayClip(std::shared_ptr<AnimationClip> clip)
 {
     if (clip == current_clip_) return false;
-    current_index_ = 0;
     current_clip_ = clip;
+    is_playing_ = true;
+    timer_ = 0.f;
+    current_index_ = 0;
     return true;
 }
