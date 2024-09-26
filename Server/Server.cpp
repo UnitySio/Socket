15713bfc6d2aa0fac98dae69eba004213a18512f
UnitySio <dnwls010728@gmail.com>
2024-09-26 14:02:22 +0900

    TEST: MySQL 테스트

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,10 +2,34 @@
 #include <iostream>
 
 #include "jdbc/mysql_driver.h"
+#include "jdbc/cppconn/resultset.h"
+#include "jdbc/cppconn/statement.h"
 
 int main()
 {
-    sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+    try
+    {
+        sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+
+        const std::string host_name = "tcp://own-game-engine.cdyyimi0op9h.ap-southeast-2.rds.amazonaws.com";
+        const std::string user_name = "admin";
+        const std::string password = "dnwls3388";
+
+        std::unique_ptr<sql::Connection> connection(driver->connect(host_name, user_name, password));
+        connection->setSchema("socket_db");
+
+        std::unique_ptr<sql::Statement> statement(connection->createStatement());
+        std::unique_ptr<sql::ResultSet> result_set(statement->executeQuery("SELECT * FROM account_info"));
+        while (result_set->next())
+        {
+            std::cout << "id: " << result_set->getInt("id") << ", password: " << result_set->getString("password") << std::endl;
+        }
+    }
+    catch (sql::SQLException e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
