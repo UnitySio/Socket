f6f725b43dca06f0b939f0adae2623751cfa4a94
dnwls010728 <dnwls010728@gmail.com>
2024-10-10 12:03:50 +0900

    FEAT: MAIN 매크로 제거

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,14 +3,13 @@
 
 #include "Client.h"
 #include "Core.h"
-#include "Misc/EngineMacros.h"
 
 #include <crtdbg.h>
 #include <iostream>
 
 #include "Windows/WindowsWindow.h"
 
-MAIN
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 #ifdef _DEBUG
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
